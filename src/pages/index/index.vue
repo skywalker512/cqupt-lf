8dcbe2aea7066a03d945dab4433a392a1887942f
houzhenhong <houzhenhong@hotmail.com>
2019-05-04 13:48:17 +0800

    [提交] 首页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,73 @@
 <template>
-	<view>
-		OK
+	<view class="nav-list">
+		<view @tap="handleNavigatorTap" hover-class="none" :class="`bg-${item.color}`" class="radius shadow-blur nav-navigator padding"
+		 v-for="(item) in elements" :key="item.title">
+			<view class="nav-info">
+				<view class="nav-title">{{item.title}}</view>
+				<view class="nav-decs">{{item.decs}}</view>
+			</view>
+			<image :src="`../../static/svg/${item.image}.svg`"></image>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {}
+	export default {
+		data() {
+			return {
+				elements: [{
+						title: '我丢了卡',
+						decs: '点击即可登记丢卡信息',
+						url: 'lost/index',
+						image: '404',
+						color: 'gradual-blue',
+					},
+					{
+						title: '我拾到了卡',
+						decs: '点击即可登记拾卡信息',
+						url: 'find/index',
+						image: '302',
+						color: 'gradual-orange',
+					},
+					{
+						title: '我的信息',
+						decs: '点击即可查看个人信息',
+						url: 'info/index',
+						image: '200',
+						color: 'gradual-purple',
+					},
+				],
+			}
+		}
+	}
 </script>
 
 <style lang="less">
+	.nav-list {
+		display: flex;
+		flex-wrap: wrap;
+		padding: 0px 30upx 0px;
+		justify-content: space-between;	
+		.nav-navigator {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+			width: 100%;
+			margin: 40upx 0;
+			.nav-info {
+				.nav-title {
+					font-size: 40upx;
+					line-height: 55upx;
+				}
+				.nav-decs {
+					font-size: 28upx;
+				}
+			}
+			image {
+				width: 200upx;
+				height: 160upx;
+			}
+		}
+	}
 </style>
